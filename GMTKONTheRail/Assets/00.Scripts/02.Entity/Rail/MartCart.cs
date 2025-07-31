68827ab3fdd27d587440bce30f40bec60869bdb2
Nutubit17 <koobonhoo@gmail.com>
2025-07-31 12:18:30 +0900

    script: cart and railmanagement

---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Rail/MartCart.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MartCart : MonoBehaviour
+{
+    [SerializeField] private RailManagement _railManagement;
+    [Header("Physics")]
+    [Range(0, 1), SerializeField] private float _friction = 0.5f;
+    private Transform _visual;
+    private IEnumerator _forceRoutine;
+
+    [Header("Shake Decoration")]
+    [Range(0, 1), SerializeField] private float _positionShakeness = 0.1f;
+    [Range(0.1f, 10), SerializeField] private float _positionFrequency = 2f;
+    [Space]
+    [Range(0, 20), SerializeField] private float _rotationShakeness = 5f;
+    [Range(0.1f, 10), SerializeField] private float _rotationFrequency = 3f;
+
+    private void Awake()
+    {
+        _visual = transform.Find("Visual");
+    }
+
+    // test
+    void Start()
+    {
+        AddForce(1);
+    }
+
+    public void AddForce(float force)
+    {
+        if (_forceRoutine != null)
+            StopCoroutine(_forceRoutine);
+
+        _forceRoutine = ForceMoveRoutine(force);
+        StartCoroutine(_forceRoutine);
+    }
+
+    private IEnumerator ForceMoveRoutine(float force)
+    {
+        float frictionDelta = Mathf.Pow(1 - _friction, Time.fixedDeltaTime);
+
+        while (Mathf.Abs(force) > 0.1f)
+        {
+            MoveUpdate(force * Time.fixedDeltaTime);
+            force *= frictionDelta;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private void MoveUpdate(float step)
+    {
+        _railManagement.AddDistance(step);
+        _railManagement.GetPositionAndDirection(out var position, out var direction);
+
+        float shakeRotation = 0;
+        ShakeDecoration(ref position, ref shakeRotation);
+
+        transform.position = position;
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+            transform.rotation *= Quaternion.AngleAxis(shakeRotation, transform.forward);
+        }
+    }
+
+    private void ShakeDecoration(ref Vector3 position, ref float shakeRotation)
+    {
+        float currentDist = _railManagement.GetCurrentDistance();
+
+        position += _visual.right * Mathf.PerlinNoise1D(currentDist * _positionFrequency) * _positionShakeness;
+        shakeRotation = (Mathf.PerlinNoise1D((currentDist + 777.7f) * _rotationFrequency) - 0.5f) * 2 * _rotationShakeness;
+    }
+}
---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Rail/RailManagement.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Splines;
+
+[RequireComponent(typeof(SplineContainer))]
+public class RailManagement : MonoBehaviour
+{
+    /* Component */
+    private SplineContainer _splineContainer;
+
+    /* Temp */
+    private int _currentPoint = 0;
+    private float _currentAmount = 0;
+    private float _totalAmount = 0;
+    private bool _isNeedPositionUpdate = true;
+    private Vector3 _previousPosition = Vector3.zero;
+    private Vector3 _position = Vector3.zero;
+
+
+    private void Awake()
+    {
+        _splineContainer = GetComponent<SplineContainer>();
+    }
+
+
+    public float GetFullDistance() => _splineContainer.CalculateLength(0);
+    public float GetCurrentDistance() => _totalAmount;
+    public float GetCurrentPercent() => _totalAmount / GetFullDistance();
+
+    public Vector3 GetCurrentPosition()
+    {
+        _previousPosition = _position;
+
+        if (_isNeedPositionUpdate)
+        {
+            _splineContainer.Evaluate(0, GetCurrentPercent(), out var position, out var tangent, out var upvector);
+
+            _position = position;
+            _isNeedPositionUpdate = false;
+        }
+
+        return _position;
+    }
+
+    public Vector3 GetCurrentDirection() => (_position - _previousPosition).normalized;
+
+    public void GetPositionAndDirection(out Vector3 position, out Vector3 direction)
+    {
+        position = GetCurrentPosition();
+        direction = GetCurrentDirection();
+    }
+
+
+    public void AddDistance(float amount)
+    {
+        _isNeedPositionUpdate = true;
+        int splineCount = _splineContainer.Spline.Count;
+
+        while (true)
+        {
+            float currentIdxDistance = Vector3.Distance(
+                _splineContainer.Spline[_currentPoint].Position,
+                 _splineContainer.Spline[_currentPoint + 1].Position);
+
+            _currentAmount += amount;
+            _totalAmount += amount;
+            _totalAmount = Mathf.Clamp(_totalAmount, 0, GetFullDistance());
+
+            if (_currentAmount < 0)
+            {
+                if (_currentPoint == 0 && _currentAmount <= 0) break;
+
+                _currentPoint = Mathf.Max(0, _currentPoint - 1);
+                float previousIdxDistance = Vector3.Distance(
+                    _splineContainer.Spline[_currentPoint].Position,
+                    _splineContainer.Spline[_currentPoint + 1].Position);
+
+                amount = _currentAmount;
+                _currentAmount = previousIdxDistance;
+            }
+            else if (_currentAmount < currentIdxDistance)
+            {
+                break;
+            }
+            else
+            {
+                if (_currentPoint == splineCount - 2 && _currentAmount >= currentIdxDistance) break;
+
+                _currentPoint = Mathf.Min(splineCount - 2, _currentPoint + 1);
+                amount = _currentAmount - currentIdxDistance;
+                _currentAmount = 0;
+            }
+        }
+    }
+}

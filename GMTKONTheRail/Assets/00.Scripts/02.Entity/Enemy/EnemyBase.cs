03f6ff636bf3a04ac405d0e1b5cad7d99405c79f
Nutubit17 <koobonhoo@gmail.com>
2025-07-31 19:50:20 +0900

    feat: lantern(light)

--- GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/EnemyBase.cs
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/EnemyBase.cs
@@ -1,8 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBase : Agent
 {
-    
+    [SerializeField] private PlayerBash _player; 
+    [SerializeField] private float _playerDetectRange = 10f;
+    [SerializeField] private float _lightDetectRange = 5f;
+    [SerializeField] private LayerMask _lightLayer;
+
+    private Collider[] _lightColliderCheckArr = new Collider[10];
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _player = FindAnyObjectByType<PlayerBash>();
+    }
+
+    public void Update()
+    {
+        bool isPlayerInRange = (_player.transform.position - transform.position).sqrMagnitude
+                         < _playerDetectRange * _playerDetectRange;
+
+        int LightCount = Physics.OverlapSphereNonAlloc
+            (transform.position, _lightDetectRange, _lightColliderCheckArr, _lightLayer);
+
+        bool isLightExists = false;
+
+        for (int i = 0; i < LightCount; ++i)
+        {
+            if (_lightColliderCheckArr[i].TryGetComponent<LightObject>(out var light))
+            {
+                if (light.IsLightOn)
+                {
+                    isLightExists = true;
+                    break;
+                }
+            }
+        }
+
+        if (isPlayerInRange && !isLightExists)
+        {
+            EnemyUpdate();
+        }
+        else
+        {
+            NormalUpdate();
+        }
+        
+    }
+
+
+    private void EnemyUpdate()
+    {
+
+    }
+
+    private void NormalUpdate()
+    {
+        
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _playerDetectRange);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _lightDetectRange);
+    }
+#endif
 }
---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Light/LightObject.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LightObject : MonoBehaviour
+{
+    [SerializeField] private LightDataSO _lightData;
+    [SerializeField] private float _fuel = 3.9f;
+    [SerializeField] private float _maxFuel = 5f;
+    [SerializeField] private float _fuelUseSpeed = 0.1f;
+
+    [Header("Light settings")]
+    [SerializeField] private Light _lightComponent = null;
+    [SerializeField] private MeshRenderer _meshRenderer = null;
+    private float _randomValue = 0;
+    private float _lightDefaultIntensity = 0;
+    private Color _DefaultEmissionColor = Color.white;
+    private readonly int _emissionColorHash = Shader.PropertyToID("_EmisiionColor");
+    [SerializeField, Range(0, 1)] private float _lightActivePercentage = 1;
+    [SerializeField, Range(0f, 0.5f)] private float _transitionSmoothness = 0.1f;
+    [SerializeField, Range(0f, 10f)] private float _frequency = 2f;
+
+    private bool _isLightOn = false;
+    public bool IsLightOn => _isLightOn;
+
+    private void Awake()
+    {
+        _lightDefaultIntensity = _lightComponent.intensity;
+        _DefaultEmissionColor = _meshRenderer.material.GetColor(_emissionColorHash);
+        _randomValue = Random.Range(0, 777.7f);
+        UpdateLightData(_fuel);
+    }
+
+    public void AddFuel(float value)
+    {
+        _fuel = Mathf.Clamp(_fuel + value, 0, _maxFuel);
+    }
+
+    private void Update()
+    {
+        float lightness = Mathf.PerlinNoise1D((Time.time + _randomValue) * _frequency) + _lightActivePercentage;
+        _isLightOn = lightness > 0.5f;
+
+        float goalIntensity = _isLightOn ? _lightDefaultIntensity * lightness : 0;
+        Color goalColor = _isLightOn ? _DefaultEmissionColor * lightness : Color.gray;
+        _lightComponent.intensity = Mathf.Lerp(_lightComponent.intensity, goalIntensity, 1 - _transitionSmoothness);
+
+        _meshRenderer.material.SetColor(_emissionColorHash,
+            Color.Lerp(_meshRenderer.material.GetColor(_emissionColorHash),
+            goalColor,
+            1 - _transitionSmoothness));
+
+
+        if (_fuel <= 0) return;
+        float newFuelValue = _fuel - _fuelUseSpeed * Time.deltaTime;
+        if (Mathf.FloorToInt(newFuelValue) != Mathf.FloorToInt(_fuel))
+        {
+            UpdateLightData(newFuelValue);
+        }
+
+        _fuel = newFuelValue;
+
+    }
+
+    private void UpdateLightData(float newFuelValue)
+    {
+        int idx = Mathf.Clamp(Mathf.FloorToInt(newFuelValue), 0, _lightData.DataCount - 1);
+
+        LightDataSO.LightDataStruct data = _lightData.dataArr[idx];
+        
+        _lightActivePercentage = data.lightActivePercentage;
+        _transitionSmoothness = data.transitionSmoothness;
+        _frequency = data.frequency;
+    }
+}

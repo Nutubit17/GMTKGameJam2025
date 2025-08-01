286cda956732a59b43d57b9fc97353451e6c28e2
Nutubit17 <koobonhoo@gmail.com>
2025-08-01 21:52:20 +0900

    feat: enemy code/hit anim

--- GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/Childs/EnemySnake.cs
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/Childs/EnemySnake.cs
@@ -1,46 +1,13 @@
 using UnityEngine;
 
 public class EnemySnake : EnemyBase
-{
-    protected override void OnIdle()
-    {
-        if (_currentState != _previousState)
-            _animator.SetFloat(_blendHash, AnimationBlend.Idle.Snake);
-        
-        if (EnemyState.Attack == _previousState)
-            transform.position += Vector3.up * 1.5f;
-    }
+{   
+    protected override int _defaultIdleBlend => AnimationBlend.Idle.Snake;
+    protected override int _attackBlendStart => AnimationBlend.Attack.Snake;
+    protected override int _attackBlendEnd => AnimationBlend.Attack.Snake;
 
-    protected override void OnAttack()
-    {
-        _animator.SetFloat(_blendHash, AnimationBlend.Attack.Snake);
+    protected override int _idleBlendStart => AnimationBlend.Idle.Snake;
+    protected override int _idleBlendEnd => AnimationBlend.Idle.Snake;
 
-        if(_currentState != _previousState)
-            transform.position -= Vector3.up * 1.5f;
-    }
-    
-    protected override void HandleAnimationEnd()
-    {
-        _animator.SetTrigger(_IdleHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Idle.Snake);
-    }
-
-    protected override void OnWalk()
-    {
-        _animator.SetFloat(_blendHash, AnimationBlend.Walk.Snake);
-        _animator.speed = 1f;
-
-        if (EnemyState.Attack == _previousState)
-            transform.position += Vector3.up * 1.5f;
-    }
-
-    protected override void OnRun()
-    {
-        _animator.SetTrigger(_walkHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Walk.Snake);
-        _animator.speed = 1.5f;
-        
-        if (EnemyState.Attack == _previousState)
-            transform.position += Vector3.up * 1.5f;
-    }
+    protected override int _walkBlend => AnimationBlend.Walk.Snake;
 }
--- GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/Childs/EnemySpider.cs
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Enemy/Childs/EnemySpider.cs
@@ -2,44 +2,13 @@ using UnityEngine;
 
 public class EnemySpider : EnemyBase
 {
-    protected override void Awake()
-    {
-        base.Awake();
-        _animator.SetTrigger(_IdleHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Idle.Spider);
-    }
+    protected override int _defaultIdleBlend => AnimationBlend.Idle.Spider;
+    
+    protected override int _attackBlendStart => AnimationBlend.Attack.Spider;
+    protected override int _attackBlendEnd => AnimationBlend.Attack.Spider;
 
-    protected override void OnIdle()
-    {
-        if (_currentState != _previousState)
-            _animator.SetFloat(_blendHash, AnimationBlend.Idle.Spider);
-    }
+    protected override int _idleBlendStart => AnimationBlend.Idle.Spider;
+    protected override int _idleBlendEnd => AnimationBlend.Idle.Spider;
 
-    protected override void OnAttack()
-    {
-        _animator.applyRootMotion = false;
-        _animator.SetTrigger(_attackHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Attack.Spider);
-    }
-
-    protected override void HandleAnimationEnd()
-    {
-        _currentState = EnemyState.Idle;
-        
-        _animator.SetTrigger(_attackHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Attack.Spider);
-    }
-
-    protected override void OnWalk()
-    {
-        _animator.SetFloat(_blendHash, AnimationBlend.Walk.Spider);
-        _animator.speed = 1f;
-    }
-
-    protected override void OnRun()
-    {
-        _animator.SetTrigger(_walkHash);
-        _animator.SetFloat(_blendHash, AnimationBlend.Walk.Spider);
-        _animator.speed = 1.5f;
-    }
+    protected override int _walkBlend => AnimationBlend.Walk.Spider;
 }

a62ff0ab58b5443048570890b36bb84738093b5d
saironee <saironeee@gmail.com>
2025-08-01 20:17:30 +0900

    Add PlayerCameraManager

---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/00.Player/PlayerCameraManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerCameraManager : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private PlayerMoveCompo PlayerMoveComp;
+
+    [SerializeField] private float tiltAmount = 5f; // �ִ� ���� ����
+    [SerializeField] private float tiltSpeed = 5f;  // ���� �ӵ�
+
+    private Quaternion targetRotation;
+
+    void Update()
+    {
+        Vector3 localVelocity = _camera.transform.InverseTransformDirection(PlayerMoveComp.rigidCompo.linearVelocity);
+        float horizontalSpeed = localVelocity.x;
+        float tilt = Mathf.Clamp(horizontalSpeed, -1f, 1f) * tiltAmount;
+
+        targetRotation = Quaternion.Euler(0f, 0f, -tilt);
+        _camera.transform.localRotation = Quaternion.Lerp(_camera.transform.localRotation, targetRotation, Time.deltaTime * tiltSpeed);
+    }
+}

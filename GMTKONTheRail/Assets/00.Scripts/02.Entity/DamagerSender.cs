21ed2d1ed72df3cbade158357031735129ffc445
BashMing10207 <backupdate102@gmail.com>
2025-08-02 19:26:22 +0900

    DamageSender

---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/DamagerSender.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DamagerSender : MonoBehaviour, IGetCompoable, IGetDamageable,IAfterInitable
+{
+
+    public Entity Mom;
+
+    public HealthCompo MomHealth;
+
+    public void AfterInit()
+    {
+        MomHealth = Mom.GetComponent<HealthCompo>();
+    }
+
+    public void Damage(float damage, float cooldown)
+    {
+        MomHealth.Damage(damage, cooldown);
+    }
+
+    public void Damage(float damage)
+    {
+        MomHealth.Damage( damage);
+    }
+
+    public void Init(Entity agent)
+    {
+        Mom = agent;
+        
+    }
+}

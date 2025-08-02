f1228e1ea3372fd5194e4980a9765b3f2cd9c247
Nutubit17 <koobonhoo@gmail.com>
2025-08-02 19:06:47 +0900

    feat: door

--- GMTKONTheRail/Assets/00.Scripts/02.Entity/Interactive/Door.cs
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Interactive/Door.cs
@@ -4,40 +4,54 @@ using UnityEngine;
 public class Door : MonoBehaviour, IAltInteractiveable
 {
     private bool isOpen = false;
-    [SerializeField] private Collider _collider;
-    [SerializeField] private Transform _visual;
-    [SerializeField] private float _moveTime = 0.5f;
+    [SerializeField] private bool _isLocked = false;
+    [SerializeField] private Transform _pivot;
+    [SerializeField] private float _moveSpeed = 0.5f;
+    
 
     private bool _isDoorMoving = false;
     private IEnumerator _doorMoveRoutine;
-    
+
+
+    public void Unlock()
+    {
+        _isLocked = true;
+    }
 
     public void UseAltInteractive(PlayerArm plarm)
     {
-        if (!_isDoorMoving)
+        if (_isLocked)
         {
-            _doorMoveRoutine = DoorMoveRoutine(isOpen = !isOpen);
-            StartCoroutine(_doorMoveRoutine);
+            // 잠긴 소리
+            return;
         }
-        
+
+        if (!_isDoorMoving)
+            {
+                _doorMoveRoutine = DoorMoveRoutine(isOpen = !isOpen);
+                StartCoroutine(_doorMoveRoutine);
+            }
+
     }
 
     public IEnumerator DoorMoveRoutine(bool isOpen)
     {
-        Quaternion rotation = Quaternion.AngleAxis(Time.deltaTime * (isOpen ? 1 : -1), Vector3.up);
         float percent = 0;
 
         _isDoorMoving = true;
-        _collider.enabled = false;
+
+        Vector3 begin = Vector3.zero;
+        Vector3 end = Vector3.up * 90;
+
+        if (!isOpen) // swap
+            (begin, end) = (end, begin);
 
         while (percent <= 1)
-        {
-            transform.rotation *= rotation;
-
-            percent += _moveTime * Time.deltaTime;
-            yield return null;
-        }
-        _collider.enabled = true;
+            {
+                _pivot.eulerAngles = Vector3.Lerp(begin, end, percent);
+                percent += _moveSpeed * Time.deltaTime;
+                yield return null;
+            }
 
         _isDoorMoving = false;
     }

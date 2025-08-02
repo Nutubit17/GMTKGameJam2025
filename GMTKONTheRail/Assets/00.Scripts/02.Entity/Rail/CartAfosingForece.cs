5fff4fe849f4d072cdbb0ddd8f545663b804179a
BashMing10207 <backupdate102@gmail.com>
2025-08-02 18:32:51 +0900

    Cart

---
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Rail/CartAfosingForece.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CartAfosingForece : MonoBehaviour
+{
+    [SerializeField]
+    private MartCart _martCart;
+    [SerializeField]
+    private float _forceMultipier =1;
+
+    private void Start()
+    {
+        _martCart = GetComponent<MartCart>();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Interactive"))
+            return;
+        _martCart.AddForce(-collision.impulse * _forceMultipier);
+        //_martCart.AddForce((transform.position - collision.collider.transform.position).normalized *_forceMultipier);
+
+    }
+}

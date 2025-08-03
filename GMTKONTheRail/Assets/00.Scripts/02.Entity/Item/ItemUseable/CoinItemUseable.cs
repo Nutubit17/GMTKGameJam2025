28dd3501562f5683d2fc0993acb188f472677830
BashMing10207 <backupdate102@gmail.com>
2025-08-04 02:53:17 +0900

    Revert "Merge branch 'main' of https://github.com/Nutubit17/GMTKGameJam2025"
    This reverts commit 229766402c5bcafa56e3694d35af3bffe0717c82, reversing
    changes made to 5d9fe48e8183d0f536a0f2af019219048f3b3633.
    
    # Conflicts:
    #	GMTKONTheRail/Assets/Scenes/Main.unity

--- GMTKONTheRail/Assets/00.Scripts/02.Entity/Item/ItemUseable/CoinItemUseable.cs
+++ GMTKONTheRail/Assets/00.Scripts/02.Entity/Item/ItemUseable/CoinItemUseable.cs
@@ -4,30 +4,6 @@ public class CoinItemUseable : ItemUseableObject
 {
     [SerializeField]
     private LayerMask _whatIsInteractive;
-
-    [SerializeField]
-    public GameObject UIUI;
-
-    public override void Update()
-    {
-        base.Update();
-        bool sdaf= false;
-
-
-        if (Physics.Raycast(transform.position, transform.forward, out var hit, 2.2f, _whatIsInteractive))
-        {
-            if (hit.transform.gameObject.TryGetComponent<ICoinUseable>(out ICoinUseable a))
-                sdaf = true;
-
-
-        }
-        UIUI.SetActive(sdaf);
-    }
-
-    protected override void OnDisable()
-    {
-        UIUI.SetActive(false);
-    }
     public void UseCoin()
     {
         if (Physics.Raycast(transform.position, transform.forward, out var hit, 2.2f, _whatIsInteractive))

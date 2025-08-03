5da8af131ce916848fe82e774a56a013783adaa8
Nutubit17 <koobonhoo@gmail.com>
2025-08-03 21:14:08 +0900

    complete: introscene

--- GMTKONTheRail/Assets/00.Scripts/NextScene.cs
+++ GMTKONTheRail/Assets/00.Scripts/NextScene.cs
@@ -1,13 +1,34 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextScene : MonoBehaviour
 {
     [SerializeField] private String _nextSceneName;
-    
+    [SerializeField] private Image _image;
+
+
+    public IEnumerator routine()
+    {
+        float percent = 0;
+        var group = _image.GetComponent<CanvasGroup>();
+
+        while (percent <= 1)
+        {
+            group.alpha = percent;
+            _image.rectTransform.position += Vector3.up * 4 * Time.deltaTime;
+
+            percent += Time.deltaTime * 0.75f;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(_nextSceneName);
+    }
+
     public void NextSceneMingMing()
     {
-        SceneManager.LoadScene(_nextSceneName);
+        StartCoroutine(routine());
     }
 }
